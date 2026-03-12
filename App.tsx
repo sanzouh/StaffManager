2ee8b7f5d9e1b977468f7762d232008f3ce04554
sanzouh <santaherizo2.0@gmail.com>
2026-03-12 20:38:51 +0300

    feat: setup nativewind tailwind css #1

--- App.tsx
+++ App.tsx
@@ -2,29 +2,22 @@ import { supabase } from "@/utils/supabase";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
-	useEffect(() => {
+	/* 	useEffect(() => {
 		async function test() {
 			const { data, error } = await supabase.from("employe").select("*");
 			console.log(data, error);
 		}
 		test();
-	}, []);
+	}, []); */
 
 	return (
-		<View style={styles.container}>
-			<Text>Hello !</Text>
-			<StatusBar style="auto" />
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-xl font-bold text-blue-500">
+				Welcome to Nativewind!
+			</Text>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
